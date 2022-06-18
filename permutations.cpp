15dbb50d4839a348e492f58412f3094a94001a09
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2022-06-18 11:10:10 -0300

    Fix formatation

--- permutations.cpp
+++ permutations.cpp
@@ -3,28 +3,26 @@ using namespace std;
 
 vector<int> per = {0, 1, 2, 3, 4, 5};
 int inf = 0;
-int sup = per.size() -1;
+int sup = per.size() - 1;
 
-void permuta(int inf, int sup) {
-	if (inf == sup) {
-		for (auto i : per) cout << i << " ";
-		cout << endl;
-		return;
-	}
-	for (int i = inf; i < sup; i++) {
-		int temp = per[i];
-		per[i] = per[inf];
-		per[inf] = temp;
-		permuta(inf + 1, sup);
-		temp = per[i];
-		per[i] = per[inf];
-		per[inf] = temp;
-	}
-
+void permutations(int inf, int sup) {
+    if (inf == sup) {
+        for (auto i : per) cout << i << " ";
+        cout << endl;
+        return;
+    }
+    for (int i = inf; i < sup; i++) {
+        int temp = per[i];
+        per[i] = per[inf];
+        per[inf] = temp;
+        permutations(inf + 1, sup);
+        temp = per[i];
+        per[i] = per[inf];
+        per[inf] = temp;
+    }
 }
 
-
 int main() {
-	permuta(inf, sup);
-	return 0;
+    permutations(inf, sup);
+    return 0;
 }
--- subsets.cpp
+++ subsets.cpp
@@ -5,19 +5,19 @@ int n;
 vector<int> sub;
 
 void subsets(int k) {
-	if (k == n+1) {
-		for (auto s : sub) cout << s << " ";
-		cout << endl;
-		return;
-	}
-	sub.push_back(k);
-	subsets(k+1);
-	sub.pop_back();
-	subsets(k+1);
+    if (k == n + 1) {
+        for (auto s : sub) cout << s << " ";
+        cout << endl;
+        return;
+    }
+    sub.push_back(k);
+    subsets(k + 1);
+    sub.pop_back();
+    subsets(k + 1);
 }
 
 int main() {
-	cin >> n;
-	subsets(1);
-	return 0;
+    cin >> n;
+    subsets(1);
+    return 0;
 }
